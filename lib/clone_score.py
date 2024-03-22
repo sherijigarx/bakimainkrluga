4c492f096fd868adfccc8dabbc85ca8145446eda
shaukathussain <33412791+IamHussain503@users.noreply.github.com>
2024-03-22 15:29:54 +0100

    Update clone_score.py

--- lib/clone_score.py
+++ lib/clone_score.py
@@ -5,7 +5,6 @@ import bittensor as bt
 from lib.reward import score
 import math
 
-
 class CloneScore:
     def __init__(self, n_mels=128):
         self.n_mels = n_mels
@@ -14,7 +13,10 @@ class CloneScore:
         waveform, sample_rate = torchaudio.load(file_path)
         mel_spectrogram_transform = T.MelSpectrogram(sample_rate=sample_rate, n_mels=self.n_mels)
         mel_spectrogram = mel_spectrogram_transform(waveform)
-        return mel_spectrogram
+        # Convert power spectrogram (amplitude squared) to decibel (dB) units
+        db_transform = T.AmplitudeToDB()
+        mel_spectrogram_db = db_transform(mel_spectrogram)
+        return mel_spectrogram_db
 
     def pad_or_trim_to_same_length(self, spec1, spec2):
         if spec1.size(2) > spec2.size(2):
@@ -29,16 +31,6 @@ class CloneScore:
         return torch.mean((spec1 - spec2) ** 2)
 
     def calculate_decay_score(self, mse_score, decay_rate):
-        """
-        Calculate decay score based on mse_score and a decay rate.
-
-        Parameters:
-        mse_score (float): The Mean Squared Error score.
-        decay_rate (float): The rate of decay, controls how fast the score decreases.
-
-        Returns:
-        float: The decay score.
-        """
         decay_score = math.exp(-decay_rate * mse_score)
         return decay_score
 
@@ -56,29 +48,31 @@ class CloneScore:
             spec1 = spec2 = None
 
         # Pad or Trim
-        spec1, spec2 = self.pad_or_trim_to_same_length(spec1, spec2)
+        if spec1 is not None and spec2 is not None:
+            spec1, spec2 = self.pad_or_trim_to_same_length(spec1, spec2)
 
-        # Calculate MSE
-        mse_score = self.calculate_mse(spec1, spec2).item()
-        bt.logging.info(f"MSE Score for Voice Cloning: {mse_score}")
+            # Calculate MSE
+            mse_score = self.calculate_mse(spec1, spec2).item()
+            bt.logging.info(f"MSE Score for Voice Cloning: {mse_score}")
 
-        # Calculate Decay Score based on MSE
-        decay_score = self.calculate_decay_score(mse_score, decay_rate)
-        bt.logging.info(f"Decay Score for Voice Cloning: {decay_score}")
+            # Calculate Decay Score based on MSE
+            decay_score = self.calculate_decay_score(mse_score, decay_rate)
+            bt.logging.info(f"Decay Score for Voice Cloning: {decay_score}")
+        else:
+            mse_score = float('inf')  # Assigning a default high value if spectrograms extraction failed
+            decay_score = 0
 
         try:
             nisqa_wer_score = score(file_path2, input_text)
         except Exception as e:
-            print(f"Error calculating NISQA score inside compare_audio function : {e}")
+            print(f"Error calculating NISQA score inside compare_audio function: {e}")
             nisqa_wer_score = 0
 
         # Calculate Final Score considering Decay Score and NISQA score
-        if nisqa_wer_score == 0:
-            final_score = 0
-        elif decay_score == 0:
+        if nisqa_wer_score == 0 or decay_score == 0:
             final_score = 0
         else:
             final_score = (decay_score + nisqa_wer_score) / 2
         bt.logging.info(f"Final Score for Voice Cloning: {final_score}")
-        
+
         return final_score
